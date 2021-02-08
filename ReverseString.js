fa9a4bae4e4596db7c455af74fd2c6b3e5dd080e
Adam Figueroa <admiralfiggins@gmail.com>
2021-02-08 09:48:22 -0800

    fixed reverse string challenge

--- ReverseString.js
+++ ReverseString.js
@@ -1,14 +1,9 @@
 // Write a function that reverses a string. Take a string as input, reverse the string, and return the new string.
 
 function stringReverser(str) {
-    if(str.length === 0) {
-        return reversedStr
-    }
-    let reversedStr = ''
-
-    for (let i = 1; i <= str.length; i++) {
-        reversedStr += str[str.length - i]
-    }
-    console.log(reversedStr)
+  if (!str) {
+    return '';
+  }
+  return stringReverser(str.slice(1)) + str[0];
 }
-stringReverser('hello')
+console.log(stringReverser('hello'));
